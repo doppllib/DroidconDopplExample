8ce0aea9949b0aca2467f09b464d8d842955e7fd
David Morant <davidm@touchlab.co>
2017-06-15 13:25:43 -0400

    Update findByCode method

--- app/src/main/java/co/touchlab/droidconandroid/shared/data2/UserAccount.java
+++ app/src/main/java/co/touchlab/droidconandroid/shared/data2/UserAccount.java
@@ -5,15 +5,13 @@ import android.arch.persistence.room.PrimaryKey;
 import android.text.TextUtils;
 
 import java.sql.SQLException;
-
-import co.touchlab.droidconandroid.shared.data.DatabaseHelper;
+import java.util.List;
 
 /**
  * Created by kgalligan on 6/28/14.
  */
 @Entity
-public class UserAccount
-{
+public class UserAccount {
     public static final String HTTPS_S3_AMAZONAWS_COM_DROIDCONIMAGES = "https://s3.amazonaws.com/droidconimages/";
 
     @PrimaryKey
@@ -49,123 +47,109 @@ public class UserAccount
 
     public Boolean emailPublic;
 
-    public Long getId()
-    {
+    public Long getId() {
         return id;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public String getProfile()
-    {
+    public String getProfile() {
         return profile;
     }
 
-    public String getUserCode()
-    {
+    public String getUserCode() {
         return userCode;
     }
 
-    public String getCompany()
-    {
+    public String getCompany() {
         return company;
     }
 
-    public String getFacebook()
-    {
+    public String getFacebook() {
         return facebook;
     }
 
-    public String getTwitter()
-    {
+    public String getTwitter() {
         return twitter;
     }
 
-    public String getLinkedIn()
-    {
+    public String getLinkedIn() {
         return linkedIn;
     }
 
-    public String getWebsite()
-    {
+    public String getWebsite() {
         return website;
     }
 
-    public boolean isFollowing()
-    {
+    public boolean isFollowing() {
         return following;
     }
 
-    public String getEmail()
-    {
+    public String getEmail() {
         return email;
     }
 
-    public String getgPlus()
-    {
+    public String getgPlus() {
         return gPlus;
     }
 
-    public String getPhone()
-    {
+    public String getPhone() {
         return phone;
     }
 
-    public String getCoverKey()
-    {
+    public String getCoverKey() {
         return coverKey;
     }
 
-    public boolean getEmailPublic()
-    {
+    public boolean getEmailPublic() {
         return emailPublic == null ? false : emailPublic;
     }
 
-    public String getAvatarKey()
-    {
+    public String getAvatarKey() {
         return avatarKey;
     }
 
-    public String avatarImageUrl()
-    {
-        if(TextUtils.isEmpty(avatarKey)) return null;
-        else if(avatarKey.startsWith("http")) return avatarKey;
+    public String avatarImageUrl() {
+        if (TextUtils.isEmpty(avatarKey)) return null;
+        else if (avatarKey.startsWith("http")) return avatarKey;
         else return HTTPS_S3_AMAZONAWS_COM_DROIDCONIMAGES + avatarKey;
     }
 
     @Override
-    public boolean equals(Object o)
-    {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         UserAccount that = (UserAccount) o;
 
-        if (avatarKey != null ? !avatarKey.equals(that.avatarKey) : that.avatarKey != null) return false;
+        if (avatarKey != null ? !avatarKey.equals(that.avatarKey) : that.avatarKey != null)
+            return false;
         if (company != null ? !company.equals(that.company) : that.company != null) return false;
         if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (linkedIn != null ? !linkedIn.equals(that.linkedIn) : that.linkedIn != null) return false;
+        if (linkedIn != null ? !linkedIn.equals(that.linkedIn) : that.linkedIn != null)
+            return false;
         if (name != null ? !name.equals(that.name) : that.name != null) return false;
         if (profile != null ? !profile.equals(that.profile) : that.profile != null) return false;
         if (twitter != null ? !twitter.equals(that.twitter) : that.twitter != null) return false;
         if (facebook != null ? !facebook.equals(that.twitter) : that.facebook != null) return false;
-        if (userCode != null ? !userCode.equals(that.userCode) : that.userCode != null) return false;
+        if (userCode != null ? !userCode.equals(that.userCode) : that.userCode != null)
+            return false;
         if (website != null ? !website.equals(that.website) : that.website != null) return false;
         if (email != null ? !email.equals(that.email) : that.email != null) return false;
         if (phone != null ? !phone.equals(that.phone) : that.phone != null) return false;
         if (gPlus != null ? !gPlus.equals(that.gPlus) : that.gPlus != null) return false;
-        if (coverKey != null ? !coverKey.equals(that.coverKey) : that.coverKey != null) return false;
-        if (emailPublic != null ? ! emailPublic.equals(that.emailPublic) : that.emailPublic != null) return false;
+        if (coverKey != null ? !coverKey.equals(that.coverKey) : that.coverKey != null)
+            return false;
+        if (emailPublic != null ? !emailPublic.equals(that.emailPublic) : that.emailPublic != null)
+            return false;
 
         return true;
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (profile != null ? profile.hashCode() : 0);
@@ -184,12 +168,10 @@ public class UserAccount
         return result;
     }
 
-    public static UserAccount findByCode(DatabaseHelper databaseHelper, String code) throws SQLException
-    {
-//        Dao<UserAccount> dao = databaseHelper.getUserAccountDao();
-//        List<UserAccount> list = new Where(dao).eq("userCode", code).query().list();
-//        return list.size() == 0 ? null : list.get(0);
-        return null;
+    public static UserAccount findByCode(DatabaseHelper databaseHelper, String code) throws SQLException {
+        // This method probably doesn't have to be in this class.
+        List<UserAccount> list = databaseHelper.getUserAccountDao().getUsersWithCode(code);
+        return list.size() == 0 ? null : list.get(0);
     }
 
 }
