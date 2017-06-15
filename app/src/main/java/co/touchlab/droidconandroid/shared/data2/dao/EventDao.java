8ad3f40c1f7968d561a52aebc919a145c3c03d38
David Morant <davidm@touchlab.co>
2017-06-15 11:03:04 -0400

    Translate Event transactions to Room

--- app/src/main/java/co/touchlab/droidconandroid/shared/data2/dao/EventDao.java
+++ app/src/main/java/co/touchlab/droidconandroid/shared/data2/dao/EventDao.java
@@ -1,8 +1,31 @@
 package co.touchlab.droidconandroid.shared.data2.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+import co.touchlab.droidconandroid.shared.data2.Event;
 
 @Dao
 public interface EventDao {
+
+    @Query("SELECT * FROM Event WHERE id = :eventId")
+    Event getEventForId(int eventId);
+
+    @Query("SELECT * FROM Event")
+    List<Event> getEvents();
+
+    @Query("SELECT * FROM Event WHERE rsvpUuid NOT NULL")
+    List<Event> rsvpUuidNotNull();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void createOrUpdate(Event event);
+
+    @Update
+    void updateEvent(Event event);
 
 }
