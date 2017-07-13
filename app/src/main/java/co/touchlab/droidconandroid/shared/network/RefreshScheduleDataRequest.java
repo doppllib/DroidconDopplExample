0e28cad0602a761884ab393d2a1c44b88ef25428
David Morant <davidm@touchlab.co>
2017-07-13 16:34:04 -0400

    Clean up use of Singles

--- app/src/main/java/co/touchlab/droidconandroid/shared/network/RefreshScheduleDataRequest.java
+++ app/src/main/java/co/touchlab/droidconandroid/shared/network/RefreshScheduleDataRequest.java
@@ -4,7 +4,6 @@ import co.touchlab.droidconandroid.shared.network.dao.Convention;
 import co.touchlab.droidconandroid.shared.network.dao.EventVideoDetails;
 import retrofit.http.GET;
 import retrofit.http.Path;
-import rx.Single;
 
 /**
  * Created by kgalligan on 7/20/14.
@@ -16,10 +15,4 @@ public interface RefreshScheduleDataRequest
 
     @GET("/dataTest/eventVideoDetails/{eventId}")
     EventVideoDetails getEventVideoDetails(@Path("eventId")Long eventId);
-
-    @GET("/dataTest/scheduleData/{conventionId}")
-    Single<Convention> getScheduleDataSingle(@Path("conventionId")Integer conventionId);
-
-    @GET("/dataTest/eventVideoDetails/{eventId}")
-    Single<EventVideoDetails> getEventVideoDetailsSingle(@Path("eventId")Long eventId);
 }
