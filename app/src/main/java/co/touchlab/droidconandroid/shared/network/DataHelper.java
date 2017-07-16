ff9f796e2f58ce97ad7b08e5f4af513f5563578e
Kevin Galligan <kgalligan@gmail.com>
2017-07-16 12:12:29 -0400

    More cleanup. Had left some code laying around.

--- app/src/main/java/co/touchlab/droidconandroid/shared/network/DataHelper.java
+++ app/src/main/java/co/touchlab/droidconandroid/shared/network/DataHelper.java
@@ -1,10 +1,5 @@
 package co.touchlab.droidconandroid.shared.network;
 
-import android.support.annotation.NonNull;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import javax.net.ssl.HttpsURLConnection;
 
 import retrofit2.Retrofit;
@@ -16,11 +11,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class DataHelper
 {
-    @NonNull
-    private static Gson makeGson() {
-        return new GsonBuilder().create();
-    }
-
     public static Retrofit makeRetrofit2Client(String baseUrl)
     {
         GsonConverterFactory factory = GsonConverterFactory.create();
