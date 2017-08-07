e96f47df0a3f9443d6b74c0a030925796a44ae21
sufeizhao <sufei@touchlab.co>
2017-08-07 17:04:09 -0400

    clean

--- app/src/main/java/co/touchlab/droidconandroid/shared/interactors/RsvpInteractor.java
+++ app/src/main/java/co/touchlab/droidconandroid/shared/interactors/RsvpInteractor.java
@@ -8,10 +8,8 @@ import co.touchlab.droidconandroid.shared.data.AppPrefs;
 import co.touchlab.droidconandroid.shared.data.DatabaseHelper;
 import co.touchlab.droidconandroid.shared.data.Event;
 import co.touchlab.droidconandroid.shared.network.RsvpRequest;
-import co.touchlab.droidconandroid.shared.network.dao.JustId;
 import co.touchlab.droidconandroid.shared.utils.AnalyticsEvents;
 import co.touchlab.droidconandroid.shared.utils.AnalyticsHelper;
-import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
@@ -38,39 +36,48 @@ public class RsvpInteractor
         return helper.getEventForId(eventId)
                 .filter(event -> event != null && event.rsvpUuid == null)
                 .map(event -> setRsvp(event, appPrefs.getUserUniqueUuid()))
-                .flatMapSingle(this :: updateEvent);
+                .flatMapSingle(this :: updateDatabase)
+                .flatMap(this :: updateBackend);
     }
 
     public Single<Event> removeRsvp(Long eventId)
     {
         return helper.getEventForId(eventId)
                 .filter(event -> event != null)
-                .map(event -> setRsvp(event, null)).flatMapSingle(this :: updateEvent);
+                .map(event -> setRsvp(event, null))
+                .flatMapSingle(this :: updateDatabase)
+                .flatMap(this :: updateBackend);
     }
 
-    private Single<Event> updateEvent(Event event)
+    private Single<Event> updateDatabase(Event event)
     {
         return Single.fromCallable(() ->
         {
             helper.updateEvent(event).subscribe();
-            if(event.rsvpUuid == null)
-            {
-                Observable<JustId> response = request.unRsvp(event.id, appPrefs.getUserUniqueUuid());
-                response.subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe();
-                AnalyticsHelper.recordAnalytics(AnalyticsEvents.UNRSVP_EVENT, event.getId());
-            }
-            else
-            {
-                Observable<JustId> response = request.rsvp(event.id, appPrefs.getUserUniqueUuid());
-                response.subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe();
-                AnalyticsHelper.recordAnalytics(AnalyticsEvents.RSVP_EVENT, event.getId());
-            }
             return event;
         });
+    }
+
+    private Single<Event> updateBackend(Event event)
+    {
+        if(event.rsvpUuid == null)
+        {
+            request.unRsvp(event.id, appPrefs.getUserUniqueUuid())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe();
+            AnalyticsHelper.recordAnalytics(AnalyticsEvents.UNRSVP_EVENT, event.getId());
+        }
+        else
+        {
+            request.rsvp(event.id, appPrefs.getUserUniqueUuid())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe();
+            AnalyticsHelper.recordAnalytics(AnalyticsEvents.RSVP_EVENT, event.getId());
+        }
+
+        return Single.just(event);
     }
 
     @NonNull
