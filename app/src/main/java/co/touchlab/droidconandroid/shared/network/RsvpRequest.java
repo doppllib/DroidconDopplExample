9aedf318e23c07302d152bf2192ca320a2ec7473
Kevin Galligan <kgalligan@gmail.com>
2017-08-07 12:51:12 -0400

    RSVP calls

--- app/src/main/java/co/touchlab/droidconandroid/shared/network/RsvpRequest.java
+++ app/src/main/java/co/touchlab/droidconandroid/shared/network/RsvpRequest.java
@@ -1,14 +1,19 @@
 package co.touchlab.droidconandroid.shared.network;
 
 
+import co.touchlab.droidconandroid.shared.network.dao.JustId;
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
  * Created by kgalligan on 7/20/14.
  */
 public interface RsvpRequest {
-    @GET("/droidconsponsers/{fileName}")
-    Observable<SponsorsResult> getSponsors(@Path("fileName") String fileName);
+    @POST("/dataTest/rsvpEventAnon/{id}")
+    Observable<JustId> rsvp(@Path("id") Long eventId);
+
+    @POST("/dataTest/unRsvpEventAnon/{id}")
+    Observable<JustId> unRsvp(@Path("id") Long eventId);
 }
