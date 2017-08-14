435c7c3a2cff7bb4efeb068c1106d900b673f527
David Morant <davidm@touchlab.co>
2017-08-14 11:48:13 -0400

    Fix agenda not updating when event rsvp'd

--- app/src/main/kotlin/co/touchlab/droidconandroid/EventDetailFragment.kt
+++ app/src/main/kotlin/co/touchlab/droidconandroid/EventDetailFragment.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import co.touchlab.droidconandroid.R.id.*
 import co.touchlab.droidconandroid.shared.data.Event
 import co.touchlab.droidconandroid.shared.data.EventInfo
 import co.touchlab.droidconandroid.shared.data.Track
