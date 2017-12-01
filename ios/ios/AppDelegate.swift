6915050a1ee59ed360892cd20f5c0f6c69dc0944
Kevin Galligan <kgalligan@gmail.com>
2017-11-30 23:15:28 -0500

    Update plugin to 0.10.3

--- ios/ios/AppDelegate.swift
+++ ios/ios/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         DopplRuntime.start()
         
